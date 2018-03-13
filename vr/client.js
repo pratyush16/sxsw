9609ded4286fc11054985acc9eff6e2794c544f6
edoardoluna <edoardo.luna002gmail.com>
2018-03-13 17:20:48 -0500

    commit

--- vr/client.js
+++ vr/client.js
@@ -8,6 +8,7 @@ import {VRInstance} from 'react-vr-web';
 function init(bundle, parent, options) {
   const vr = new VRInstance(bundle, 'sxswhack', parent, {
     // Add custom options here
+    cursorVisibility: 'visible',
     ...options,
   });
   vr.render = function() {
