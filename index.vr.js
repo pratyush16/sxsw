45f0309a6af97c42b040f8c73a30f110107e1989
edoardoluna <edoardo.luna002gmail.com>
2018-03-13 17:09:54 -0500

    commit

--- index.vr.js
+++ index.vr.js
@@ -7,28 +7,36 @@ import {
   View,
 } from 'react-vr';
 
-export default class sxswhack extends React.Component {
+class sxswhack extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerState: new MediaPlayerState({autoPlay: true, muted: true}), // init with muted, autoPlay
+    };
+  }
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
+        <Pano source={asset('chess-world.jpg')} />
+        <View
           style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
+            alignItems: 'center',
+            layoutOrigin: [0.5, 0.5, 0],
+            transform: [{translate: [0, 0, -4]}],
           }}>
-          hello
-        </Text>
+          <Video
+            style={{height: 2.25, width: 4}}
+            source={[
+              asset('video.mp4', {format: 'mp4'}),
+              asset('video.webm', {format: 'webm'}),
+            ]}
+            playerState={this.state.playerState}
+          />
+          <VideoControl style={{height: 0.2, width: 4}} playerState={this.state.playerState} />
+        </View>
       </View>
     );
   }
-};
+}
 
-AppRegistry.registerComponent('sxswhack', () => sxswhack);
+AppRegistry.registerComponent('VideoSample', () => VideoSample);
