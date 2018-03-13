66f9d079f2d409fed5511ff434542b6f1671f9b3
Pratyush Potu <31892913+pratyush16@users.noreply.github.com>
2018-03-13 17:46:09 -0500

    Update index.vr.js

--- index.vr.js
+++ index.vr.js
@@ -20,9 +20,15 @@ class sxswhack extends React.Component {
         <Pano source={asset('chess-world.jpg')} />
         <View
           style={{
-            alignItems: 'center',
-            layoutOrigin: [0.5, 0.5, 0],
-            transform: [{translate: [0, 0, -4]}],
+            backgroundColor: '#777879',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -3]}],
           }}>
           <Video
             style={{height: 2.25, width: 4}}
